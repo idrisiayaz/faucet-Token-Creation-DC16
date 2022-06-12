c280b6cd8a02339e86399b21e6396ed4c038d144
Ayaz Idrisi <88124100+idrisiayaz@users.noreply.github.com>
2022-06-12 16:16:44 +0530

    Add files via upload

---
+++ token_assets/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="blue window" id="logo">
+        <h1>
+          <span role="img" aria-label="tap emoji">
+            ⚡
+          </span>
+          DCRYPT16 🌈 Tokens
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
